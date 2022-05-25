231800790beeb9f02840466d5192b471d39c939f
Sidanton6006 <61793320+Sidanton6006@users.noreply.github.com>
2022-05-25 10:24:42 +0300

    Added UnitOfWork

---
+++ Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork, IDisposable
+    {
+        private readonly ApplicationDbContext _ctx;
+
+        public UnitOfWork(ApplicationDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _ctx.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            _ctx.Dispose();
+        }
+    }
+}
