077541fb84c4d1d27d2edae1eb182e07280e6894
Dima <65868322+byCodeXp@users.noreply.github.com>
2022-06-09 08:20:29 +0300

    add Seed extension for WebApplication

---
+++ Web/Seed.cs
@@ -0,0 +1,37 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Shared;
+
+namespace Web;
+
+public static partial class WebApplicationExtensions
+{
+    public static void UseSeed(this WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var roleManager = scope.ServiceProvider.GetService<RoleManager<RoleEntity>>();
+
+            Seed(roleManager!).Wait();
+        }
+    }
+
+    private static async Task Seed(RoleManager<RoleEntity> roleManager)
+    {
+        await SeedCustomerRole(roleManager);
+    }
+
+    private static async Task SeedCustomerRole(RoleManager<RoleEntity> roleManager)
+    {
+        var customerRoleName = AppSettings.Roles.Customer; 
+        
+        var customerRole = new RoleEntity(customerRoleName);
+
+        var customerRoleIsNotExists = !await roleManager.RoleExistsAsync(customerRoleName);
+        
+        if (customerRoleIsNotExists)
+        {
+            await roleManager.CreateAsync(customerRole);
+        }
+    }
+}
