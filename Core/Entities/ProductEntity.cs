02fd5a7d107efa0efecf8aaaf4af4e78a7716e8f
Sidanton6006 <61793320+Sidanton6006@users.noreply.github.com>
2022-06-15 13:12:11 +0300

    Added configuration for BaseEntity

---
+++ Core/Entities/ProductEntity.cs
@@ -0,0 +1,10 @@
+﻿using Core.Entities.Abstract;
+
+namespace Core.Entities;
+
+public class Product : BaseEntity
+{
+    public string Name { get; set; };
+    public double Price { get; set; };
+    public string Description { get; set; };
+}
---
+++ Infrastructure/Data/EntitiesConfiguration/Abstract/BaseEntityConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities.Abstract;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.EntitiesConfiguration.Abstract;
+
+internal abstract class BaseEntityConfiguration<TEntity> : IEntityTypeConfiguration<TEntity> where TEntity : BaseEntity
+{
+    public virtual void Configure(EntityTypeBuilder<TEntity> builder)
+    {
+        builder.HasKey(e => e.Id);
+
+        builder.Property(e => e.CreatedAt).HasDefaultValueSql("getutcdate()").ValueGeneratedOnAdd();
+        builder.Property(e => e.UpdatedAt).HasDefaultValueSql("getutcdate()").ValueGeneratedOnAddOrUpdate();
+    }
+}
