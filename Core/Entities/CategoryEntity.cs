731773c2a782d9bbd50038f1b7d9a4e0b608725f
Sidanton6006 <61793320+Sidanton6006@users.noreply.github.com>
2022-06-22 10:27:26 +0300

    Added "CategoryEntity"

---
+++ Core/Entities/CategoryEntity.cs
@@ -0,0 +1,19 @@
+﻿using Core.Entities.Abstract;
+
+namespace Core.Entities;
+
+public class CategoryEntity : BaseEntity
+{
+    public CategoryEntity()
+    {
+        Children = new HashSet<CategoryEntity>();
+        Products = new HashSet<ProductEntity>();
+    }
+    public string Title { get; set; }
+    public string Image { get; set; }
+    public Guid? ParentId { get; set; }
+
+    public virtual CategoryEntity Parent { get; set; }
+    public virtual ICollection<CategoryEntity> Children { get; set; }
+    public virtual ICollection<ProductEntity> Products { get; set; }
+}
