510d2e76a647866dd1aaf21fa11ed6c29cd092e4
Sidanton6006 <61793320+Sidanton6006@users.noreply.github.com>
2022-06-22 12:44:58 +0300

    Fixed bugs 1

--- Core/Entities/CategoryEntity.cs
+++ Core/Entities/CategoryEntity.cs
@@ -3,17 +3,18 @@
 namespace Core.Entities;
 
 public class CategoryEntity : BaseEntity
-{
+{    
+    public string Title { get; set; }
+    public string Image { get; set; }
+    public Guid? ParentCategoryId { get; set; }
+
+    public virtual CategoryEntity Parent { get; set; }
+    public virtual ICollection<CategoryEntity> Children { get; set; }
+    public virtual ICollection<ProductEntity> Products { get; set; }
+
     public CategoryEntity()
     {
         Children = new HashSet<CategoryEntity>();
         Products = new HashSet<ProductEntity>();
     }
-    public string Title { get; set; }
-    public string Image { get; set; }
-    public Guid? ParentId { get; set; }
-
-    public virtual CategoryEntity Parent { get; set; }
-    public virtual ICollection<CategoryEntity> Children { get; set; }
-    public virtual ICollection<ProductEntity> Products { get; set; }
 }
--- Core/Entities/ProductEntity.cs
+++ Core/Entities/ProductEntity.cs
@@ -8,6 +8,6 @@ public class ProductEntity : BaseEntity
     public decimal Price { get; set; }
     public string Description { get; set; }
 
-    public int CategoryId { get; set; }
+    public Guid CategoryId { get; set; }
     public CategoryEntity Category { get; set; }
 }
